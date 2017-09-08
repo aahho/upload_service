0e34c505c74369e5c706d660b6217223d3ed7746
Ankur Mishra <ankurmishra0113@gmail.com>
2017-09-09 02:17:00 +0530

    added different responses

--- app/response.py
+++ app/response.py
@@ -1,15 +1,122 @@
 import flask
 
-def http_response(self, status_code, data, message='Success', type='success'):
+def respond_with_item(statusCode, data, transformer):
     response = {}
-    response = flask.jsonify({
-            'notification': {
-            'hint':'Reponse Sent', 
-            'message':message, 
-            'aahho_code':'SE-'+str(status_code), 
-            'type':type
-            }, 
-            'data': data
-        })
-    response.status_code = status_code
+    response['data'] = transformer.transform(data)
+
+    response['notification'] = {}
+    response['notification']['hint'] = "Response Sent"
+    response['notification']['message'] = "Success"
+    response['notification']['responseCode'] = statusCode
+    response['notification']['type'] = "Success"
+
+    response = flask.jsonify(response)
+    response.status_code = statusCode
     return response
+
+def respond_with_success(statusCode, message, hint="Response Sent"):
+    response = {}
+    response['data'] = str(message)
+    
+    response['notification'] = {}
+    response['notification']['hint'] = hint
+    response['notification']['message'] = "Success"
+    response['notification']['responseCode'] = statusCode
+    response['notification']['type'] = "Success"
+    
+    response = flask.jsonify(response)
+    response.status_code = statusCode
+    return response
+
+def respond_with_list(statusCode, data, hint="Response Sent"):
+    response = {}
+    response['data'] = data
+    
+    response['notification'] = {}
+    response['notification']['hint'] = hint
+    response['notification']['message'] = "Success"
+    response['notification']['responseCode'] = statusCode
+    response['notification']['type'] = "Success"
+    
+    response = flask.jsonify(response)
+    response.status_code = statusCode
+    return response
+
+def respond_with_transformed_list(statusCode, data, transformer):
+    response = {}
+    response['data'] = fetch_data_from_transformer(transformer, data)
+    
+    response['notification'] = {}
+    response['notification']['hint'] = "Response Sent"
+    response['notification']['message'] = "Success"
+    response['notification']['responseCode'] = statusCode
+    response['notification']['type'] = "Success"
+    
+    response = flask.jsonify(response)
+    response.status_code = statusCode
+    return response
+
+def respond_with_paginated_collection(request, statusCode, data, transformer):
+    total_length = len(data)
+    items = request.GET.get('items', '10')
+    page = request.GET.get('page', '1')
+    valid_page = total_length/int(items)
+
+    if valid_page < int(page):
+        page = 1
+
+    if int(items) <= 0:
+        items = 10
+
+    if int(page) <= 0:
+        page = 1     
+
+    pages_possible = total_length/int(page)
+
+    if not pages_possible:
+        page = 1    
+
+    url = 'http://' + request.META['HTTP_HOST'] + request.META['PATH_INFO'] + '?'
+
+    meta = {}
+    meta['total'] = total_length
+    meta['count'] = total_length
+    meta['current_page'] = page
+    meta['per_page'] = items
+    meta['next_link'] = ''
+    meta['previous_link'] = ''
+
+    paginator = Paginator(data, items)
+    results = paginator.page(page)
+
+    if results.has_next():
+        if (total_length - int(items)) < int(items):
+            new_items = total_length - int(items)
+        else:
+            new_items = items   
+        new_page = int(page) + 1
+        next_link =  url + 'items' + '=' + str(new_items) + '&' + 'page' + '=' + str(new_page)
+        meta['next_link'] = next_link
+
+    if results.has_previous:
+        page = int(page) - 1
+        if not page:
+            page = 1
+        previous_link =  url + 'items' + '=' + str(items) + '&' + 'page' + '=' + str(page)
+        meta['previous_link'] = previous_link
+
+    results = results.object_list
+
+    response = {}
+    response['data'] = fetch_data_from_transformer(transformer, results)
+    response['meta'] = meta
+
+    response = flask.jsonify(response)
+    response.status_code = statusCode
+    return response
+    
+def fetch_data_from_transformer(transformer, data):
+    result = []
+    for key, value in enumerate(data):
+        result.append(transformer.transform(value))
+    return result
--- aws_s3/urls.py
+++ aws_s3/urls.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
+from app import response
 
 aws_s3 = Blueprint('aws_s3', __name__, url_prefix='/upload')
 
 @aws_s3.route('', methods=['GET'])
 def get():
-	return 'Working'
+	return response.respond_with_success(200, 'Hello User!')
